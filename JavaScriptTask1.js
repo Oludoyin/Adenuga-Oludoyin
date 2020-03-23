6a678ffe83d67909a83159f9a2ad50782985e7e6
Adenuga Oludoyin <otegbeyeoludoyin@gmail.com>
2020-03-23 20:43:35 +0100

    javascript task1

---
+++ JavaScriptTask1.js
@@ -0,0 +1,11 @@
+var  name="Oludoyin Adenuga";
+var courses=["Html","Css","JavaScript","UI/UX","Php"];
+
+console.log(name);
+console.log(courses);
+
+for(i=0; i<=200; i++) {
+    if(i%2!==0){
+        console.log(i);
+    }
+}
